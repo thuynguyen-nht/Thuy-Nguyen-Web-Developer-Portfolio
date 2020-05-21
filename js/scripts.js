69d5cbae619a22031d9414d2d04a285210c80fad
ThuyN <thuynguyen.nht20@gmail.com>
2020-05-20 23:21:34 -0700

    update picture for portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,16 @@ $('body')
   .on('mouseenter mouseleave','.dropdown',toggleDropdown)
   .on('click', '.dropdown-menu a', toggleDropdown);
 
+$( document ).ready(function () {
+		$(".moreBox").slice(0, 3).show();
+		if ($(".blogBox:hidden").length != 0) {
+			$("#loadMore").show();
+		}		
+		$("#loadMore").on('click', function (e) {
+			e.preventDefault();
+			$(".moreBox:hidden").slice(0, 6).slideDown();
+			if ($(".moreBox:hidden").length == 0) {
+				$("#loadMore").fadeOut('slow');
+			}
+		});
+	});
